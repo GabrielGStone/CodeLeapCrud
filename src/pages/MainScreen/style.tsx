4ab200c20468e96945f9300c651979a56ff6a300
GabrielStone <anjogabriel986@hotmail.com>
2023-05-08 00:09:57 -0300

    fix layout content

--- src/pages/MainScreen/style.tsx
+++ src/pages/MainScreen/style.tsx
@@ -4,6 +4,9 @@ export const Container = styled.div`
   min-height: 100vh;
   height: 100%;
   background-color: #fff;
+  @media (max-width: 500px) {
+    width: 100%;
+  }
 `;
 
 export const PostsBody = styled.div`
